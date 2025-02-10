33c0bef9daede963c12e43f467d04af013922cef
QiYongWu <qiyongw.@126.com>
2025-02-10 23:02:54 +0800

    Generate html by amazon.js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -31,56 +31,57 @@ const products = [
     priceCents : 799 //   价格/美分
   } ];
 
-const html = `
-        <div class="product-container">
-                <div class="product-image-container">
-                  <img class="product-image"
-                    src="${products[0].image}">
-                </div>
-
-                <div class="product-name limit-text-to-2-lines">
-                  ${products[0].name}
-                </div>
-
-                <div class="product-rating-container">
-                  <img class="product-rating-stars"
-                    src="${starts_image}>"
-                  <div class="product-rating-count link-primary">
-                    ${products[0].rating.count}
-                  </div>
-                </div>
-
-                <div class="product-price">
-                  $${(products[0].count/100).toFixed(2)}  
-                </div>
-
-                <div class="product-quantity-container">
-                  <select>
-                    <option selected value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                  </select>
-                </div>
-
-                <div class="product-spacer"></div>
-
-                <div class="added-to-cart">
-                  <img src="images/icons/checkmark.png">
-                  Added
-                </div>
-
-                <button class="add-to-cart-button button-primary">
-                  Add to Cart
-                </button>
-              </div> `;
-  
-  console.log(html);
   //float.toFixed(n) 将小数float转化成字符串，并保留n位小数
+  let products_html = "";
+  products.forEach((product) => {
+    products_html += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image" src="${product.image}">
+        </div>
+  
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+  
+        <div class="product-rating-container">
+          <img class="product-rating-stars" src="${product.rating.starts_image}">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+  
+        <div class="product-price">
+          $${(product.priceCents / 100).toFixed(2)}
+        </div>
+  
+        <div class="product-quantity-container">
+          <select>
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+  
+        <div class="product-spacer"></div>
+  
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+  
+        <button class="add-to-cart-button button-primary">
+          Add to Cart
+        </button>
+      </div>
+    `;
+  });
 
+  console.log(products_html);
